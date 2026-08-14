b8c8ad95b4c718902da57798f44a74b057ca3ffb
Divya shah <shahdivya680@gmail.com>
2026-08-14 13:52:40 +0530

    Add C++ practice programs

---
+++ 2nd.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int t =8;
+    int p =4;
+    int s =t/p;
+    cout << "total number is " << s << endl;
+    return 0;
+}
---
+++ sum.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a=10;
+    int b=34;
+    cout << "sum of a and b is = " << a+b << endl;
+    return 0;
+}
