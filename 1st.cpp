6211d4cdc5b6055826296931f54d662445a3b2e5
Divya shah <divyashah@Divyas-MacBook-Air.local>
2026-08-14 12:11:32 +0530

    initial commit

---
+++ 1st.cpp
@@ -0,0 +1,17 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    int t =8;
+    int p =4;
+    int s =t/p;
+    cout << "total number is " << s << endl;
+    int d = t*p;
+    cout << "total ans is " << d <<endl;
+    int a =t+p;
+    cout << "total add is " << a <<endl;
+    int m =t-p;
+    cout <<"total difference is " << m <<endl;
+    
+    return 0;
+}
